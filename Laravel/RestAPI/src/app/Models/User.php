049ce3f12c193fe7043257499d10c0adcc91378f
letien-kimei <tienlm@kimei.vn>
2025-02-20 17:02:20 +0700

    rest api

--- Laravel/RestAPI/src/app/Models/User.php
+++ Laravel/RestAPI/src/app/Models/User.php
@@ -12,6 +12,15 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    // public function attendees() {
+    //     return $this->hasMany(Attendees::class);
+    // }
+
+    // public function event() {
+    //     return $this->hasMany(Event::class);
+    // }
+
+
     /**
      * The attributes that are mass assignable.
      *
